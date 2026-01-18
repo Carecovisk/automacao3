bbc40c9f0f7938c0ab7f91e23430cd696a86d3fa
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2026-01-17 23:22:49 -0300

    Organizando estrutura

---
+++ frontend/static/js/home.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('paste', function (e) {
+    e.preventDefault();
+    const statusDiv = document.getElementById('status');
+
+    // 1. Pega o HTML sujo (pagina inteira)
+    const clipboardHTML = e.clipboardData.getData('text/html');
+    if (!clipboardHTML) {
+        statusDiv.textContent = "Erro: Nenhum HTML encontrado. Você copiou do site da secretaria?";
+        return;
+    }
+
+    statusDiv.textContent = "Processando dados...";
+
+    // 2. Parser do HTML na memória
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(clipboardHTML, "text/html");
+    console.log("Documento colado:", doc);
+
+    // 3. Encontra conteudo relevante
+    const content = doc.getElementById('conteudo')
+    const description = doc.querySelector('html body div#pagina table tbody tr td table tbody tr td table tbody tr td table tbody tr td div#conteudo fieldset strong form table tbody tr td strong')
+        ?.textContent.trim() || "Sem descrição disponível.";
+    console.log("Descrição da tabela:", description);
+
+    const bestTable = doc.querySelector('html body div#pagina table tbody tr td table tbody tr td table tbody tr td table tbody tr td div#conteudo fieldset strong form table:nth-of-type(2)');
+    const maxRows = bestTable ? bestTable?.rows?.length : 0;
+
+
+    statusDiv.textContent = `Sucesso! Extraída a tabela com ${maxRows} linhas.`;
+    // 5. Converte a "Melhor Tabela" usando SheetJS
+    const workbook = XLSX.utils.table_to_book(bestTable, { raw: true });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    console.log(worksheet);
+    // Gera o JSON
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }); // header:1 traz array de arrays
+
+    document.getElementById('output').textContent = JSON.stringify(jsonData, null, 2);
+
+});
